a6cb51b3ce4be2a706ec0473ab7a2c0fdda44646
Abdelrhman Anas <abdo2008anas@gmail.com>
2025-11-24 21:03:48 +0200

    finishing account page

--- data/user-data.js
+++ data/user-data.js
@@ -1,5 +1,5 @@
-export let userName;
-export let TotalMoney;
-export let country = JSON.parse(localStorage.getItem('country')) || '$';
-export let lang;
-export let currency;
+export let userName = JSON.parse(localStorage.getItem('userName')) || '';
+export let TotalMoney = JSON.parse(localStorage.getItem('TotalMoney')) || '0.00';
+export let country = JSON.parse(localStorage.getItem('country')) || 'eg';
+export let language = JSON.parse(localStorage.getItem('language')) || 'us';
+export let currency = JSON.parse(localStorage.getItem('currency')) || 'EGP';
--- scripts/account.js
+++ scripts/account.js
@@ -1,19 +1,28 @@
 import { countries } from '../data/countries.js';
 import { languages } from '../data/language.js';
-import { userName, TotalMoney, country, lang, currency } from '../data/user-data.js';
+import { userName, TotalMoney, country, language, currency } from '../data/user-data.js';
 //data variables
-let us;
 let to;
 let co;
 let la;
 let cu;
 // elements inside the html
+//selectors
 const counElement = document.querySelector('.viewer-coun-div');
 const counResult = document.querySelector('.selector-coun-div');
 const langELement = document.querySelector('.viewer-lang-div');
 const langResult = document.querySelector('.selector-lang-div');
 const currELement = document.querySelector('.viewer-curr-div');
 const currResult = document.querySelector('.selector-curr-div');
+//inputs
+const nameInput = document.querySelector('.name-input');
+const netInput = document.querySelector('.net-input');
+//buttons
+const subButton = document.querySelector('.js-button');
+//error Elements
+const error1 = document.querySelector('.js-1');
+const error2 = document.querySelector('.js-2');
+const error3 = document.querySelector('.js-3');
 //click tracker
 document.addEventListener('click', (e) => {
   if (!(e.target === counResult)) {
@@ -24,7 +33,7 @@ document.addEventListener('click', (e) => {
   };
   if (!(e.target === currResult)) {
     currELement.innerHTML = '';
-  }
+  };
 });
 //country selector
 counResult.addEventListener('click', () => {
@@ -45,7 +54,7 @@ counResult.addEventListener('click', () => {
       countries.forEach((coun) => {
         if (coun.code === buttonValue) {
           counData = coun;
-        }
+        };
       });
       co = counData.code;
       console.log(co);
@@ -112,5 +121,43 @@ currResult.addEventListener('click', () => {
     });
   })   
 });
-
-
+//submit button
+subButton.addEventListener('click', () => {
+  error1.innerHTML = '';
+  error2.innerHTML = '';
+  error3.innerHTML = '';
+  const nameValue = nameInput.value.trim();
+  const moneyValue = netInput.value.trim();
+  if (nameValue.length === 0 || nameValue === undefined || nameValue === null) {
+    error1.innerHTML = 'Please put a name.'
+  };
+  if (moneyValue.length === 0 || moneyValue === null || moneyValue === undefined) {
+    to = 0;
+  } else {
+    to = moneyValue;
+  };
+  if (co === undefined) {
+    error2.innerHTML = 'Please choose a country.';
+  };
+  if (la === undefined) {
+    la = 'us'
+  };
+  if (cu === undefined) {
+    error3.innerHTML = 'please choose a currency.'
+  };userName, TotalMoney, country, language, currency
+  if (co && la && cu && to && (nameValue.length > 0)) {
+    userName = nameValue;
+    TotalMoney = to;
+    country = co;
+    language = la;
+    currency = cu;
+    localStorage.setItem('userName', JSON.stringify(userName));
+    localStorage.setItem('TotalMoney', JSON.stringify(TotalMoney));
+    localStorage.setItem('country', JSON.stringify(country));
+    localStorage.setItem('language', JSON.stringify(language));
+    localStorage.setItem('currency', JSON.stringify(currency));
+    console.log('finished');
+  } else {
+    console.log('failed');
+  };
+});
